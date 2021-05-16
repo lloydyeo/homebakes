72dddb19f9a3fe374c7d769869fa404a0f619456
Lloyd Yeo <lloydyeo@firstcom.com.sg>
2021-05-17 00:49:22 +0800

    - Modified from post method to any

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('subscribe', [HomeController::class, 'subscribe']);
+Route::any('subscribe', [HomeController::class, 'subscribe']);
