f64a63163c3a9cf7d9dac22d76f0cf8a97a70ac6
Lloyd Yeo <lloydyeo@firstcom.com.sg>
2021-05-23 21:50:20 +0800

    - Added copy

--- webpack.mix.js
+++ webpack.mix.js
@@ -28,6 +28,7 @@ mix.webpackConfig({
 });
 
 mix.js('resources/js/app.js', 'public/js');
+mix.js('resources/js/homepage.js', 'public/js');
 mix.copy('resources/css/colors.css', 'public/css');
 mix.copyDirectory('resources/images', 'public/images');
 mix.copy('resources/canvas/demos/restaurant/images/sketch-header.png', 'public/canvas/css/images');
