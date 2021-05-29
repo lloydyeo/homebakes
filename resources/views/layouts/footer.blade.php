961600ee9302e87574861320034e73a5b726e560
Lloyd Yeo <lloydyeo@firstcom.com.sg>
2021-05-29 22:12:35 +0800

    - Modified footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,6 +2,21 @@
     <div id="copyrights" class="bg-transparent pb-4">
         <div class="container clearfix">
             <div class="row justify-content-between col-mb-30">
+
+                <div class="col-12 col-md-auto text-center text-md-start">
+                    <div class="d-flex justify-content-center justify-content-md-start">
+                        <a href="https://www.facebook.com/HomebakersSingapore/" target="_blank" class="social-icon si-small si-colored si-facebook">
+                            <i class="icon-facebook"></i>
+                            <i class="icon-facebook"></i>
+                        </a>
+
+                        <a href="https://www.instagram.com/homebakerssingapore" target="_blank" class="social-icon si-small si-colored si-instagram">
+                            <i class="icon-instagram"></i>
+                            <i class="icon-instagram"></i>
+                        </a>
+                    </div>
+                </div>
+
                 <div class="col-12 col-md-auto text-center text-md-left">
                     <span>&copy; HomeBakers 2021. All Rights Reserved.</span>
                 </div>
