3f4c4b9ddc1507b8ca5aeb08afcab0739feccb71
Lloyd Yeo <lloydyeo@firstcom.com.sg>
2021-06-11 23:33:55 +0800

    - Modify to use middleware trustproxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Fideloper\Proxy\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 use Closure;
 
@@ -21,11 +22,11 @@ class TrustProxies extends Middleware
      */
     protected $headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB;
 
-//    public function handle(Request $request, Closure $next) {
-//        $request->setTrustedProxies([$request->getClientIp()], $this->headers);
-//        if (!$request->secure()) {
-//            return redirect()->secure($request->getRequestUri());
-//        }
-//        return $next($request);
-//    }
+    public function handle(Request $request, Closure $next) {
+        $request->setTrustedProxies([$request->getClientIp()], $this->headers);
+        if (!$request->secure()) {
+            return redirect()->secure($request->getRequestUri());
+        }
+        return $next($request);
+    }
 }
