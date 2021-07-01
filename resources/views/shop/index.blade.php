e499fc5bcdfaee4839e1773edf9ba9a197b3e92a
Lloyd Yeo <lloydyeo@firstcom.com.sg>
2021-07-01 21:56:45 +0800

    - Modified to s3

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -23,7 +23,7 @@
                         <div class="col-xl-4 col-md-6">
                             <div class="card border-0 dark shadow-sm h-shadow shadow-ts p-3" style="
                             background: linear-gradient(rgba(62,38,28,.8), rgba(62,38,28,.4)),
-                            url('{{ Storage::disk('public')->url($shop->cover_image) }}') no-repeat center center / contain;">
+                            url('{{ Storage::disk('s3')->url($shop->cover_image) }}') no-repeat center center / contain;">
                                 <div class="card-body">
 {{--                                    <h6 class="font-weight-normal text-uppercase ls2 text-white-50 mb-2">Category</h6>--}}
                                     <h3 class="font-secondary card-title h2 font-weight-bold mb-4 text-white">{{ $shop->name }}</h3>
